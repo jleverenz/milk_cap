a7a90a3cbb5a133047ebce5912628f628da8ff78
Jeff Leverenz <jeff.leverenz@gmail.com>
2017-03-10 23:53:54 -0600

    Replace deprecated use of RSpec::Core::Configuration#color_enabled=

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,6 @@ require 'rspec'
 require 'milk_cap'
 
 RSpec.configure do |config|
-  config.color_enabled = true
-  config.formatter     = 'documentation'
+  config.color = true
+  config.formatter = 'documentation'
 end
